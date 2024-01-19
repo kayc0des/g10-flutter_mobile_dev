e25e50038e6055754b7d38a3b2242d7df4523f35
Obedine-Flore <f.obedine@alustudent.com>
2024-01-19 10:31:03 +0100

    commit

---
+++ assignment_1/heelo_world_flore/android/app/src/main/kotlin/com/example/heelo_world_flore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.heelo_world_flore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ assignment_1/heelo_world_flore/lib/main.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: avoid_print
+  print("Hello World");
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World")
+          )
+      )
+    )
+  );
+}
